649e281e3747fc35d37f60c2f93541447ff84cb4
jih189 <jih189@ucsd.edu>
2023-05-10 17:18:22 -0700

    include the code to predict grasp poses

--- src/manipulation/scripts/pickupplace.py
+++ src/manipulation/scripts/pickupplace.py
@@ -8,6 +8,18 @@ import random
 from coppeliasim_zmq.srv import AttachObjectToGripper, DetachObjectToGripper
 from object_detector.srv import detect2DObject, detect2DObjectRequest
 
+import tf2_ros
+from ros_tensorflow_msgs.srv import *
+from rail_segmentation.srv import *
+from rail_manipulation_msgs.srv import *
+from geometry_msgs.msg import TransformStamped
+from manipulation_test.srv import *
+
+from sensor_msgs.msg import CameraInfo
+import tf
+from ros_numpy import numpify
+import numpy as np
+
 
 def get_zmq_clients():
 
@@ -42,7 +54,22 @@ class Manipulation(object):
 
         if isSim:
             self.attach_client, self.detach_client = get_zmq_clients()
+
+        # init a table searcher
+        rospy.wait_for_service('table_searcher/search_table')
+        self.table_searcher = rospy.ServiceProxy('table_searcher/search_table', SearchTable)
+
+        # init table-top object searcher
+        rospy.wait_for_service('table_searcher/segment_objects')
+        self.object_searcher = rospy.ServiceProxy('table_searcher/segment_objects', SegmentObjects)
+
+        # init grasp predictor
+        rospy.wait_for_service('grasp_predict')
+        self.grasp_predictor = rospy.ServiceProxy('grasp_predict', Predict)
         
+        # get camera transform from tf tree
+        self.tfBuffer = tf2_ros.Buffer()
+        self.listener = tf2_ros.TransformListener(self.tfBuffer)
 
         rospy.spin()
 
@@ -59,6 +86,40 @@ class Manipulation(object):
         
         center_x, center_y, size_x, size_y = detection.bbox.center.x, detection.bbox.center.y, detection.bbox.size_x, detection.bbox.size_y
 
+        table_info = self.table_searcher()
+        detected_objects = self.object_searcher()
+
+        # need to get the camera matrix
+        camera_info = rospy.wait_for_message('/head_camera/rgb/camera_info', CameraInfo)
+        camera_matrix = np.reshape(camera_info.K, (3, 3))
+
+        # get the camera pose
+        try:
+            camera_trans = self.tfBuffer.lookup_transform('base_link', 'head_camera_rgb_optical_frame', rospy.Time())
+        except (tf2_ros.LookupException, tf2_ros.ConnectivityException, tf2_ros.ExtrapolationException):
+            print("tf error")        
+
+        select_object_id = 0
+        has_selected_object = False
+        for i in range(len(detected_objects.segmented_objects.objects)):
+            object_center = np.array([detected_objects.segmented_objects.objects[i].center.x, 
+                                      detected_objects.segmented_objects.objects[i].center.y, 
+                                      detected_objects.segmented_objects.objects[i].center.z])
+            object_center_in_cam = np.dot(np.linalg.inv(numpify(camera_trans.transform)), np.append(object_center, 1))[:3]
+            
+            if is_in_box(object_center_in_cam, camera_matrix, boundary[0], boundary[1], boundary[2], boundary[3]):
+                select_object_id = i
+                has_selected_object = True
+                break
+
+        # if there is no object selected.
+        if not has_selected_object:
+            return
+
+        try:
+            predicted_grasp_result = self.grasp_predictor(table_info.full_point_cloud, detected_objects.segmented_objects.objects[select_object_id].point_cloud, camera_trans)
+        except rospy.ServiceException as e:
+            print("Service call failed: %s"%e)
 
         # perform manipulation
         rospy.loginfo("Picking up ")
@@ -84,6 +145,25 @@ class Manipulation(object):
         place_as_result.success = random.choice([True, False])
         rospy.loginfo("Placed object!")
         self.place_as.set_succeeded(place_as_result)
+
+    def project_3d_to_2d(self, point_3d, intrinsic_matrix):
+
+        # Project the 3D point to 2D using the intrinsic matrix
+        point_2d_homogeneous = np.dot(intrinsic_matrix, point_3d)
+
+        # Normalize the homogeneous 2D point to obtain the Euclidean 2D point
+        point_2d = point_2d_homogeneous / point_2d_homogeneous[2]
+
+        return point_2d[:2]
+
+    def is_in_box(self, point_3d, intrinsic_matrix, min_x, max_x, min_y, max_y):
+        point_2d = project_3d_to_2d(point_3d, intrinsic_matrix)
+        u = point_2d[0]
+        v = point_2d[1]
+        if(u < min_x or u > max_x or v < min_y or v > max_y):
+            return False
+        else:
+            return True
 
 
 def create_manipulation_clients():
