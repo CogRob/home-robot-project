885feaa1062e47f24a13ac177bf27371c0b33e24
ShrutheeshIR <shrutheesh99@gmail.com>
2023-05-17 14:26:58 -0700

    roomgraph

--- src/tidy_module/scripts/tidy_services.py
+++ src/tidy_module/scripts/tidy_services.py
@@ -8,6 +8,8 @@ from home_robot_msgs.msg import RoomReceptacle, ObjectLocation
 from vision_msgs.msg import Detection2D, BoundingBox2D, ObjectHypothesisWithPose
 from object_detector.srv import detect2DObject, detect2DObjectRequest
 from semantic_localization.srv import SemanticLocalizer, SemanticLocalizerRequest
+import numpy as np
+
 
 
 class TidyModule(object):
@@ -30,6 +32,73 @@ class TidyModule(object):
         )
         self.semantic_localize_client.wait_for_service()
 
+        self.kg_dict = self.get_kg_dict(data_path = "/catkin_ws/src/tidy_module/data/housekeep.npy")
+
+    def get_kg_dict(self, data_path):
+        # data_path = 'housekeep.npy'
+        data_dict = np.load(data_path, allow_pickle=True, encoding='utf-8').item()
+
+        objects = data_dict['objects']
+        rooms = data_dict['rooms']
+        room_receps = data_dict['room_receptacles']
+        data = data_dict['data']
+
+        our_object_list = sorted(["master_chef_can", "cracker_box", "sugar_box", "mustard_bottle", "tomato_soup_can", 
+                                "mug", "potted_meat_can", "banana", "bleach_cleanser", "gelatin_box", "foam_brick"])
+
+        our_room_list = sorted(["kitchen", "dining_room", "living_room", "corridor", "home_office"])
+        mergeable_rooms = {"pantry_room": "kitchen", "lobby": "corridor", "storage_room": "kitchen"}
+
+        our_recep_list = ['chair', 'coffee_machine', 'coffee_table', 'counter', 'shelf', 'sofa', 'table']
+        mergeable_receps = {"sofa_chair": "sofa", "office_chair": "chair"}
+
+
+        kg = {}
+        for id in range(len(data)):
+            row = data.loc[id]
+
+            object_name = objects[row['object_idx']]
+            if object_name not in our_object_list:
+                continue
+            elif object_name not in kg.keys():
+                kg[object_name] = {}
+
+            room_name = rooms[row['room_idx']]
+            if room_name not in our_room_list:
+                if room_name not in mergeable_rooms.keys():
+                    continue
+                else:
+                    room_name = mergeable_rooms[room_name]
+
+            correct_receps = [room_receps[r].split('|')[1] for r in row['correct']]
+            if not correct_receps:
+                continue
+
+            if room_name not in kg[object_name].keys():
+                kg[object_name][room_name] = {}
+
+            for recep_rank, recep in enumerate(correct_receps):
+                if recep not in our_recep_list:
+                    if recep not in mergeable_receps.keys():
+                        continue
+                    else:
+                        recep = mergeable_receps[recep]
+
+                if recep not in kg[object_name][room_name].keys():
+                    kg[object_name][room_name][recep] = len(correct_receps)-recep_rank
+                else:
+                    kg[object_name][room_name][recep]+= len(correct_receps)-recep_rank
+
+        for object_name, room_dict in kg.items():
+            for room_name, recep_dict in room_dict.items():
+                kg[object_name][room_name] = {k: v for k, v in sorted(kg[object_name][room_name].items(), key=lambda item: item[1], reverse=True)}
+                kg[object_name][room_name]["total"] = sum(kg[object_name][room_name].values())
+            kg[object_name] = {k: v for k, v in sorted(kg[object_name].items(), key=lambda item: item[1]['total'], reverse=True)}
+        
+        return kg
+    
+
+
     def id_misplaced_objects_cb(self, request):
         # rgb_image = request.rgbd_image.rgb
         # depth_image = request.rgbd_image.depth
@@ -46,10 +115,9 @@ class TidyModule(object):
 
         response_object = IdentifyMisplacedObjectsResponse()
         for detected_object in object_detections.detections.detections:
-            obj_loc = ObjectLocation(object_id = "mug", room = cur_room, receptacle = "office_desk")
-            # obj_loc = ObjectLocation(object_id = detected_object.object_id, room = cur_room, receptacle = "office_desk")
+            # obj_loc = ObjectLocation(object_id = "mug", room = cur_room, receptacle = "office_desk")
+            obj_loc = ObjectLocation(object_id = detected_object.object_id, room = cur_room, receptacle = "office_desk")
             response_object.object_locations.append(obj_loc)
-        print(response_object)
         return response_object
 
     def get_object_receptacles_cb(self, request):
@@ -58,13 +126,20 @@ class TidyModule(object):
         cur_receptacle = request.object_location.receptacle
         response_object = GetCorrectPlacementsResponse()
         response_object.placements.object_id = object_id
-        rooms = ['kitchen', 'living_room']
-        for room in rooms:
+
+        objkg = self.kg_dict[object_id]
+        room_recep_list = []
+        
+        for room, recep_dict in objkg.items():
+            recep_dict.pop('total')
+            room_recep_list.append([room,list(recep_dict.keys())])
+
             room_receptacles = RoomReceptacle()
             room_receptacles.room = room
-            receptacles = ["coffee table", "countertop"]
-            room_receptacles.receptacles = receptacles
+            room_receptacles.receptacles = list(recep_dict.keys())
             response_object.placements.candidates.append(room_receptacles)
+
+        print("Candidates are : ", response_object)
         return response_object
 
 
