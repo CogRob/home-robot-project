d9093130df4e349a41487ceb8efb4d0414dc5ca1
ShrutheeshIR <shrutheesh99@gmail.com>
2023-05-30 18:11:44 -0700

    fixed attach

--- src/coppeliasim/coppeliasim_zmq/scripts/attach_object.py
+++ src/coppeliasim/coppeliasim_zmq/scripts/attach_object.py
@@ -5,8 +5,13 @@ from coppeliasim_zmq.srv import AttachObjectToGripper, AttachObjectToGripperResp
 
 class ZMQServer:
     def __init__(self):
+        rospy.loginfo("-------> Initing zmq 1 \n\n")
         client = RemoteAPIClient()
+        rospy.loginfo("Got client")
         self.sim = client.getObject('sim')
+
+        rospy.loginfo("-------> Initing zmq \n\n")
+
 
         self.attach_object_to_gripper_service = rospy.Service(
             "attach_object_to_gripper_service", AttachObjectToGripper, self.attach_object_to_gripper_service_cb
@@ -14,12 +19,13 @@ class ZMQServer:
         self.detach_object_to_gripper_service = rospy.Service(
             "detach_object_to_gripper_service", DetachObjectToGripper, self.detach_object_to_gripper_service_cb
         )
+        rospy.loginfo("-------> Inited zmq \n\n")
 
         rospy.spin()
 
     def attach_object_to_gripper_service_cb(self, request):
         obj_name = request.object_id
-        obj_handle = self.sim.getObject("/" + obj_name)
+        obj_handle = self.sim.getObject("/" + obj_name + "_respondable")
         gripper_handle = self.sim.getObject("/gripper_attachment")
         val = self.sim.setObjectParent(obj_handle, gripper_handle, True)
 
@@ -27,7 +33,7 @@ class ZMQServer:
 
     def detach_object_to_gripper_service_cb(self, request):
         obj_name = request.object_id
-        obj_handle = self.sim.getObject("/" + obj_name)
+        obj_handle = self.sim.getObject("/" + obj_name + "_respondable")
         val = self.sim.setObjectParent(obj_handle, -1, True)
 
         return DetachObjectToGripperResponse(success=True)
@@ -35,6 +41,6 @@ class ZMQServer:
 
 if __name__ == '__main__':
     rospy.init_node("zmq_server")
-    rospy.loginfo("Inited!")
     zmq_srv = ZMQServer()
+    rospy.loginfo("Inited!")
     rospy.spin()
