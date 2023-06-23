5d2d7d0616389dbba963c1d912484f5f262a6e15
ShrutheeshIR <shrutheesh99@gmail.com>
2023-06-22 21:47:06 -0700

    drawer functionality complete

--- src/manipulation/scripts/pickup_place_dummy_sim.py
+++ src/manipulation/scripts/pickup_place_dummy_sim.py
@@ -2,7 +2,7 @@
 
 import rospy
 import actionlib
-from manipulation.msg import PickupAction, PlaceAction, PickupResult, PlaceResult, SetJointsToActuateAction, SetJointsToActuateResult
+from manipulation.msg import PickupAction, PlaceAction, PickupResult, PlaceResult, OpenDrawerAction, OpenDrawerResult, CloseDrawerAction, CloseDrawerResult, SetJointsToActuateAction, SetJointsToActuateResult
 import random
 from local_path_planner.msg import moveHeadAction, moveHeadGoal
 
@@ -49,51 +49,43 @@ class Manipulation(object):
         self.move_fetch_head_client.wait_for_server()
 
 
-        self.place_as = actionlib.SimpleActionServer("place_server", PlaceAction, execute_cb=self.pickup_cb, auto_start = False)
+        self.place_as = actionlib.SimpleActionServer("place_server", PlaceAction, execute_cb=self.place_cb, auto_start = False)
         self.place_as.start()
 
         self.prepare_manip_as = actionlib.SimpleActionServer("prepare_manipulation_joints", SetJointsToActuateAction, execute_cb=self.prepare_manip_cb, auto_start = False)
         self.prepare_manip_as.start()
 
+        self.open_drawer_as = actionlib.SimpleActionServer("open_drawer_server", OpenDrawerAction, execute_cb=self.open_drawer_cb, auto_start = False)
+        self.open_drawer_as.start()
+        self.close_drawer_as = actionlib.SimpleActionServer("close_drawer_server", CloseDrawerAction, execute_cb=self.close_drawer_cb, auto_start = False)
+        self.close_drawer_as.start()
 
-        if isSim:
-            self.attach_client, self.detach_client = get_zmq_clients()
-        self.torso_controller_client = actionlib.SimpleActionClient(
-            "/torso_controller/follow_joint_trajectory",
-            FollowJointTrajectoryAction,
-        )
+
+        # if isSim:
+        #     self.attach_client, self.detach_client = get_zmq_clients()
+        # self.torso_controller_client = actionlib.SimpleActionClient(
+        #     "/torso_controller/follow_joint_trajectory",
+        #     FollowJointTrajectoryAction,
+        # )
         
         rospy.spin()
+
+    def open_drawer_cb(self, request):
+        print(request)
+        res = OpenDrawerResult()
+        self.open_drawer_as.set_succeeded(res)
+
+    def close_drawer_cb(self, request):
+        print(request)
+        res = CloseDrawerResult()
+        self.close_drawer_as.set_succeeded(res)
+
 
     def pickup_cb(self, request):
 
         object_id = request.object_id
 
-        # call perception
-        detections = self.object_detector_client()
-
-        for detection in detections.detections.detections:
-            if detection.object_id == object_id:
-                break
-        
-        center_x, center_y, size_x, size_y = detection.bbox.center.x, detection.bbox.center.y, detection.bbox.size_x, detection.bbox.size_y
-
-
-        # perform manipulation
-        rospy.loginfo("Picking up ")
-        print(object_id)
-
-        # Change this
         pickup_as_result = PickupResult()
-
-        move_head_joints = moveHeadGoal()
-        move_head_joints.joint_values = [0.0, 0.25]
-
-        rospy.loginfo("Turning head")
-        self.move_fetch_head_client.send_goal(move_head_joints)
-        self.move_fetch_head_client.wait_for_result()
-
-
         # self.attach_client(object_id=object_id)
         pickup_as_result.success = True
         rospy.loginfo("Picked up!")
@@ -108,26 +100,26 @@ class Manipulation(object):
                         'forearm_roll_joint',
                         'wrist_flex_joint',
                         'wrist_roll_joint']
-        joint_values = [-1.3089969389957472, -0.08726646259971647, -2.897246558310587, 1.3962634015954636, -1.8151424220741028, 1.8151424220741028, 1.1868238913561442]
-        current_values = self.move_group.get_current_joint_values()
-        self.move_group.set_joint_value_target(joint_values)
-        plan = self.move_group.go()
+        # joint_values = [-1.3089969389957472, -0.08726646259971647, -2.897246558310587, 1.3962634015954636, -1.8151424220741028, 1.8151424220741028, 1.1868238913561442]
+        # current_values = self.move_group.get_current_joint_values()
+        # self.move_group.set_joint_value_target(joint_values)
+        # plan = self.move_group.go()
 
-        msg = FollowJointTrajectoryGoal()
-        msg.trajectory.header.frame_id = ''
-        msg.trajectory.joint_names = ['torso_lift_joint']
+        # msg = FollowJointTrajectoryGoal()
+        # msg.trajectory.header.frame_id = ''
+        # msg.trajectory.joint_names = ['torso_lift_joint']
 
-        point = JointTrajectoryPoint()
-        point.positions = [0.35]
-        point.time_from_start = rospy.Duration(1.0)
+        # point = JointTrajectoryPoint()
+        # point.positions = [0.35]
+        # point.time_from_start = rospy.Duration(1.0)
 
-        msg.trajectory.header.stamp = rospy.Time.now()
-        msg.trajectory.points = []
-        msg.trajectory.points.append(point)
+        # msg.trajectory.header.stamp = rospy.Time.now()
+        # msg.trajectory.points = []
+        # msg.trajectory.points.append(point)
 
-        self.torso_controller_client.send_goal_and_wait(
-            msg, execute_timeout=rospy.Duration(2.0)
-        )
+        # self.torso_controller_client.send_goal_and_wait(
+        #     msg, execute_timeout=rospy.Duration(2.0)
+        # )
 
         self.prepare_manip_as.set_succeeded(SetJointsToActuateResult(success=True))
 
@@ -142,7 +134,7 @@ class Manipulation(object):
         # Change this
 
         place_as_result = PlaceResult()
-        place_as_result.success = random.choice([True, False])
+        place_as_result.success = True
         rospy.loginfo("Placed object!")
         self.place_as.set_succeeded(place_as_result)
 
